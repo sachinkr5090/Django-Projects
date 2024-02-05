9ca730a127245aa24cc6fd35c9dd443437be21a7
sachinkr <Sachin.Kumar@cantire.com>
2024-02-05 09:25:07 +0530

    Add python anywhere for hosting

--- my-first-blog/mysite/settings.py
+++ my-first-blog/mysite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
 
 # Application definition
