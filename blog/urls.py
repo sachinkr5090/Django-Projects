e5ca52a0830ed3db0487d389cd84af5f3597074a
sachinkr <Sachin.Kumar@cantire.com>
2024-02-07 18:35:18 +0530

    Add new page

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views # importing views from blog application
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
